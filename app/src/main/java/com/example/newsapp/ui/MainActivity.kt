2ca5118ae0268148935674c94cf83a47d4b7f86a
rustem <200103424@stu.sdu.edu.kz>
2023-05-05 17:51:52 +0600

    Добавил фронтенд

--- app/src/main/java/com/example/newsapp/ui/MainActivity.kt
+++ app/src/main/java/com/example/newsapp/ui/MainActivity.kt
@@ -1,12 +1,53 @@
 package com.example.newsapp.ui
 
-import androidx.appcompat.app.AppCompatActivity
+import android.content.Intent
 import android.os.Bundle
+import android.view.View
+import androidx.appcompat.app.AppCompatActivity
+import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.fragment.findNavController
+import androidx.navigation.ui.setupWithNavController
 import com.example.newsapp.R
+import com.google.android.material.bottomnavigation.BottomNavigationView
+import com.example.newsapp.databinding.ActivityMainBinding
+import com.example.newsapp.ui.fragments.ArticleFragment
+import com.example.newsapp.ui.fragments.BreakingNewsFragment
+import com.example.newsapp.ui.fragments.SavedNewsFragment
+import com.example.newsapp.ui.fragments.SearchNewsFragment
+
 
 class MainActivity : AppCompatActivity() {
+    lateinit var bottomNavigationView: BottomNavigationView
+    lateinit var newsNavHostFragment: NavHostFragment
+    private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        replaceFragment(BreakingNewsFragment())
+
+        binding.bottomNavigationView.setOnItemSelectedListener{
+
+            when(it.itemId){
+                R.id.breakingNewsFragment -> replaceFragment(BreakingNewsFragment())
+                R.id.savedNewsFragment -> replaceFragment(SavedNewsFragment())
+                R.id.searchNewsFragment -> replaceFragment(SearchNewsFragment())
+                else ->{
+
+                }
+            }
+            true
+
+        }
+
+    }
+
+    private fun replaceFragment(fragment: Fragment){
+        val fragmentManager = supportFragmentManager
+        val fragmentTransaction = fragmentManager.beginTransaction()
+        fragmentTransaction.replace(R.id.frameLayoutFragment, fragment)
+        fragmentTransaction.commit()
     }
 }
---
+++ app/src/main/java/com/example/newsapp/ui/api/NewsAPI.kt
@@ -0,0 +1,5 @@
+package com.example.newsapp.ui.api
+
+interface NewsAPI {
+
+}
