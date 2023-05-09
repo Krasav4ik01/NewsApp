eeb523d5547ab192c96263cdc441de3f2d948386
rustem <200103424@stu.sdu.edu.kz>
2023-05-09 19:02:59 +0600

    ProviderFactory

--- app/src/main/java/com/example/newsapp/ui/MainActivity.kt
+++ app/src/main/java/com/example/newsapp/ui/MainActivity.kt
@@ -5,28 +5,36 @@ import android.os.Bundle
 import android.view.View
 import androidx.appcompat.app.AppCompatActivity
 import androidx.fragment.app.Fragment
+import androidx.lifecycle.ViewModelProvider
 import androidx.navigation.fragment.NavHostFragment
 import androidx.navigation.fragment.findNavController
 import androidx.navigation.ui.setupWithNavController
 import com.example.newsapp.R
 import com.google.android.material.bottomnavigation.BottomNavigationView
 import com.example.newsapp.databinding.ActivityMainBinding
+import com.example.newsapp.ui.db.ArticleDatabase
 import com.example.newsapp.ui.fragments.ArticleFragment
 import com.example.newsapp.ui.fragments.BreakingNewsFragment
 import com.example.newsapp.ui.fragments.SavedNewsFragment
 import com.example.newsapp.ui.fragments.SearchNewsFragment
+import com.example.newsapp.ui.repository.NewsRepository
 
 
 class MainActivity : AppCompatActivity() {
     lateinit var bottomNavigationView: BottomNavigationView
     lateinit var newsNavHostFragment: NavHostFragment
     private lateinit var binding: ActivityMainBinding
+    lateinit var newsViewModel: NewsViewModel
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
         replaceFragment(BreakingNewsFragment())
+
+        val repository = NewsRepository(ArticleDatabase(this))
+        val viewModelProviderFactory = NewsViewModelProviderFactory(repository)
+        newsViewModel = ViewModelProvider(this, viewModelProviderFactory).get(NewsViewModel::class.java)
 
         binding.bottomNavigationView.setOnItemSelectedListener{
 
--- app/src/main/java/com/example/newsapp/ui/fragments/ArticleFragment.kt
+++ app/src/main/java/com/example/newsapp/ui/fragments/ArticleFragment.kt
@@ -1,8 +1,18 @@
 package com.example.newsapp.ui.fragments
 
+import android.os.Bundle
+import android.view.View
 import androidx.fragment.app.Fragment
 import com.example.newsapp.R
+import com.example.newsapp.ui.MainActivity
+import com.example.newsapp.ui.NewsViewModel
 
 class ArticleFragment: Fragment(R.layout.fragment_article) {
+    lateinit var newsViewModel: NewsViewModel
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        newsViewModel = (activity as MainActivity).newsViewModel
+    }
 
 }
